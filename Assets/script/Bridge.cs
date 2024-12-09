c8e3450b4e140ee8c82c3c041cb26dbaee95c981
Erick Yuri Pereira <132232819+erickyuri159@users.noreply.github.com>
2024-12-08 21:53:10 -0300

    att

--- Assets/script/Bridge.cs
+++ Assets/script/Bridge.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Bridge : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private HingeJoint2D hingeJoint;
+    public HingeJoint2D hingeJoint;
     public float dropSpeed = 5.0f; // Velocidade com que a ponte cai
+    public Vector2 finalPosition; // Posição final da ponte
 
     void Start()
     {
@@ -21,9 +23,15 @@ public class Bridge : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.GetComponent<TilemapCollider2D>() != null)
         {
-            rb.isKinematic = true; // Torna a ponte cinemática novamente quando atinge o chão
+            rb.isKinematic = true; // Torna a ponte cinemática novamente quando atinge o Tilemap Collider 2D
+            rb.velocity = Vector2.zero; // Para o movimento da ponte
+            rb.angularVelocity = 0f; // Para a rotação da ponte
+            transform.position = finalPosition; // Define a posição final da ponte
+            transform.rotation = Quaternion.identity; // Reseta a rotação da ponte
         }
     }
 }
+
+
